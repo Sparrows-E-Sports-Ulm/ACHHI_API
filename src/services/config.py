b4f81ad9227f3aa85387f75b91e38ced32b2d6f1
JustinMuecke <justin.muecke@gmail.com>
2025-10-21 22:11:01 +0200

    feat(services): Finished config service

--- src/services/config.py
+++ src/services/config.py
@@ -1,5 +1,55 @@
-from pyhocon import ConfigFactory
+from pyhocon import ConfigFactory, ConfigTree
+from dataclasses import dataclass, fields, is_dataclass
+
+def dataclass_from_config_tree(ct: ConfigTree, data_class: dataclass): 
+    args= [
+        typed_value_from_config_tree(ct, field.type, field.name)
+        for field in fields(data_class)
+    ]
+
+def typed_value_from_config_tree(hocon: ConfigTree, field_type: type, field_name: str):
+    if field_type == str:
+        return hocon.get_string(field_name)
+    if field_type == int: 
+        return hocon.get_int(field_name)
+    if is_dataclass(field_type):
+        sub_config = hocon.get_config(field_name)
+        return dataclass_from_config_tree(sub_config, field_type)
+
+
+
+
+
+
+
+@dataclass
+class Database:
+    host: str
+    port: int 
+    user: str
+    password: str
+
+
 
 class Config():
-    def __init__(self, config_file):
-        conf = ConfigFactory.parse_file("resources/application.conf")
+    database: Database
+    def __init__(self, config_file=None):
+        hocon = ConfigFactory.parse_file(config_file)
+
+        for field_obj in fields(Config):
+            field_name = field_obj.name
+            field_type = field_obj.type
+
+            if hocon.get(field_name, None) is not None: 
+                try:
+                    config_subtree = hocon.get_config(field_name)
+                    setattr(
+                        self,
+                        field_name,
+                        dataclass_from_config_tree(config_subtree, field_type)
+                    )
+                except Exception as e:
+                    setattr(self, field_name, None)
+                    raise ValueError(f"{field_name} not found")
+            else: 
+                setattr(self, field_name, None)
